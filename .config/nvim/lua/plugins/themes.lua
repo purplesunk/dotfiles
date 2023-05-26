967c9866ebfc1adbf9164e122abf75ee25cf3000
purplesunk <84647408+purplesunk@users.noreply.github.com>
2023-05-25 23:59:44 -0300

    colorscheme config

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -5,9 +5,54 @@ return {
     },
 
     {
-		"catppuccin/nvim",
-		name = "catppuccin",
+        "catppuccin/nvim",
+        name = "catppuccin",
         lazy = false,
         priority = 1000,
+        config = function ()
+            require("catppuccin").setup({
+                flavour = "macchiato", -- latte, frappe, macchiato, mocha
+                background = { -- :h background
+                    light = "latte",
+                    dark = "macchiato",
+                },
+                transparent_background = true,
+                show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+                term_colors = false,
+                dim_inactive = {
+                    enabled = false,
+                    shade = "dark",
+                    percentage = 0.15,
+                },
+                no_italic = false, -- Force no italic
+                no_bold = false, -- Force no bold
+                no_underline = false, -- Force no underline
+                styles = {
+                    comments = {},
+                    conditionals = {},
+                    loops = {},
+                    functions = {},
+                    keywords = {},
+                    strings = {},
+                    variables = {},
+                    numbers = {},
+                    booleans = {},
+                    properties = {},
+                    types = {},
+                    operators = {},
+                },
+                color_overrides = {},
+                custom_highlights = {},
+                integrations = {
+                    cmp = true,
+                    -- gitsigns = true,
+                    -- nvimtree = true,
+                    telescope = true,
+                    notify = false,
+                    mini = false,
+                    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+                },
+            })
+        end,
 	},
 }
