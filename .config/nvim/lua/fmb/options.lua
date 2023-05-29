fd091812c574e51dbea9b71510894f425d330683
purplesunk <84647408+purplesunk@users.noreply.github.com>
2023-05-29 13:46:05 -0300

    Global status and winbar with name of file

--- .config/nvim/lua/fmb/options.lua
+++ .config/nvim/lua/fmb/options.lua
@@ -28,7 +28,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.smartindent = true
 
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = 'no'
 
 vim.o.wrap = true
 
@@ -52,6 +52,10 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
+
+vim.o.laststatus = 3
+
+vim.o.winbar = "%=%m %f%="
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
