272ce655bb71d199908d98ce19f3cde4278cc5c7
purplesunk <84647408+purplesunk@users.noreply.github.com>
2023-05-29 00:20:20 -0300

    remove ;

--- .config/nvim/lua/fmb/plugin_configs/init.lua
+++ .config/nvim/lua/fmb/plugin_configs/init.lua
@@ -1,7 +1,7 @@
-require('fmb.plugin_configs.catppuccin');
-require('fmb.plugin_configs.lsp');
-require('fmb.plugin_configs.cmp');
-require('fmb.plugin_configs.treesitter');
-require('fmb.plugin_configs.telescope');
-require('fmb.plugin_configs.autopairs');
-require('fmb.plugin_configs.neo-tree');
+require('fmb.plugin_configs.catppuccin')
+require('fmb.plugin_configs.lsp')
+require('fmb.plugin_configs.cmp')
+require('fmb.plugin_configs.treesitter')
+require('fmb.plugin_configs.telescope')
+require('fmb.plugin_configs.autopairs')
+require('fmb.plugin_configs.neo-tree')
