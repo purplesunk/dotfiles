8d1ee52b3a120ef5c65fbdbcb7f60e9c6ea182ce
purplesunk <84647408+purplesunk@users.noreply.github.com>
2023-06-01 18:25:25 -0300

    comment

--- .config/nvim/lua/fmb/keymaps.lua
+++ .config/nvim/lua/fmb/keymaps.lua
@@ -16,7 +16,7 @@ vim.keymap.set("n", "<S-h>", ":bprevious<CR>")
 -- Move windows with Ctrl and h, j, k, l
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-k>", "<C-w>k") -- lsp change this
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- Move page up and down with cursor in the middle
