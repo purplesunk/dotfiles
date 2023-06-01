9d94867ed25c81afc34f8efd064edc436af00833
purplesunk <84647408+purplesunk@users.noreply.github.com>
2023-06-01 18:26:17 -0300

    for windows?

--- .config/nvim/lua/fmb/options.lua
+++ .config/nvim/lua/fmb/options.lua
@@ -67,3 +67,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+vim.env.PATH = vim.env.PATH .. (is_windows and ";" or ":") .. vim.fn.stdpath "data" .. "/mason/bin"
