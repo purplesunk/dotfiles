b5c21402870e99234403b4e27223605bf7f7f852
purplesunk <84647408+purplesunk@users.noreply.github.com>
2023-06-18 23:47:20 -0300

    Neotree to the right

--- .config/nvim/lua/plugins/configs/neo-tree.lua
+++ .config/nvim/lua/plugins/configs/neo-tree.lua
@@ -23,7 +23,7 @@ require('neo-tree').setup {
         },
     },
     window = {
-        position = "left",
+        position = "right",
         width = 30,
         mapping_options = {
             noremap = true,
