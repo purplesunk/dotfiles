311ed03d59d4ce5c1afd62002d70734ac8f044b1
purplesunk <84647408+purplesunk@users.noreply.github.com>
2023-07-11 23:09:34 -0300

    Little change

--- .local/bin/newpipe-newsboat.sh
+++ .local/bin/newpipe-newsboat.sh
@@ -2,5 +2,5 @@
 
 sed -E 's/(.*\[\{|\}\]\})/ /g' "$1" | \
 sed -E 's/\},\{/\n/g' | \
-sed -E 's/^.*"url":"(https.*)","?name":"(.*)".?$/\1 ! \"Youtube Subscriptions\" # "~\2"/gm' | \
+sed -E 's/^.*"url":"(https.*)","?name":"(.*)".?$/\1  \"Youtube\" # "~\2"/gm' | \
 sed -E 's/channel\//feeds\/videos.xml?channel_id=/gm'
